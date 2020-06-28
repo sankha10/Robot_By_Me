027378c41e716be295d273b9e25eacb46ca5dc28
Sankhasubhra Saha <sankhalm10@gmail.com>
2020-06-28 11:31:59 +0530

    Updates

---
+++ precache-manifest.c8d5324813af026e13804d22e01ce992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277a6e91d1d32f9b3262572ecfe6ee87",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "6c5b2c27f9f8d2ac846b",
+    "url": "/robot/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "1f17a2a36b509846fef5",
+    "url": "/robot/static/css/main.c52b1b62.chunk.css"
+  },
+  {
+    "revision": "6c5b2c27f9f8d2ac846b",
+    "url": "/robot/static/js/2.660a8600.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robot/static/js/2.660a8600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f17a2a36b509846fef5",
+    "url": "/robot/static/js/main.d46a2265.chunk.js"
+  },
+  {
+    "revision": "b8c6c8734e43ce3e41e5",
+    "url": "/robot/static/js/runtime-main.32e3b786.js"
+  }
+]);
